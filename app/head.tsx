2dd2f97bf0751270c9fdce0e99830476faea7e99
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2022-12-09 18:26:17 -0600

    #corrección

--- app/head.tsx
+++ app/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
 
-      <meta name="autor" content="Jose Angel Hdz Rda" />
+      <meta name="author" content="Jose Angel Hdz Rda" />
       <meta name="keywords" content="proyectos, portafolio, jose, angel, hdz, rda" />
       <meta name="description" content="portafolio para mostrar mis proyectos jose angel hdz rda" />
     </>
