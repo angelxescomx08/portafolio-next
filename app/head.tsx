fe8c72dcccb452e6ba4d181e58276fbc46f1bab5
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2022-12-24 09:21:14 -0600

    Cambios para SEO

--- app/head.tsx
+++ app/head.tsx
@@ -1,14 +1,17 @@
 export default function Head() {
   return (
     <>
-      <title>Portafolio - José Ángel Hdz Rda</title>
+      <title>Portafolio - Jose Angel Hdz Rda</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
 
-      <meta charSet="UTF-8" />
+      <meta charSet="utf-8" />
       <meta name="author" content="Jose Angel Hdz Rda" />
-      <meta name="keywords" content="proyectos, portafolio, jose, angel, hdz, rda" />
-      <meta name="description" content="portafolio para mostrar mis proyectos jose angel hdz rda" />
+      <meta name="keywords" content="proyectos, portafolio, jose, angel, hdz, rda, desarrollo web, movil, programas" />
+      <meta name="description" content="portafolio para mostrar mis proyectos de desarrollo web, móvil, y programas de escritorio. Todo creado por mí José Ángel Hdz Rda" />
+
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
     </>
   )
 }
