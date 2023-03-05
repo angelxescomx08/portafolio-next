36665fc21f161675aed354e8a69d67cbf82d274c
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-04 22:12:40 -0600

    layout mejorado

--- app/components/MisProyectos/Proyecto/index.tsx
+++ app/components/MisProyectos/Proyecto/index.tsx
@@ -4,6 +4,7 @@ import { FC, useContext, useRef } from 'react';
 import Image from 'next/image'
 import { m, LazyMotion, domAnimation, useInView, motion } from 'framer-motion';
 import { ModalProyectoContext } from '../../../context/ModalProyecto/ModalProyectoContext';
+import Grid from '@mui/material/Grid';
 
 interface Props {
     id: number;
@@ -34,34 +35,36 @@ export const Proyecto: FC<Props> = ({ id, nombre, container, imagenDesktop, imag
     }
 
     return (
-        <motion.article
-            layoutId={`${id}`}
-            className={'relative z-10 w-full p-3 rounded-2xl cursor-pointer'}
-            ref={ref}
-            onClick={onClick}
-        >
-            <LazyMotion features={domAnimation}>
-                {isInView && <m.div
-                    className={'relative w-full aspect-video'}
-                    variants={container}
-                    initial="hidden"
-                    animate={isInView ? "show" : ''}
-                >
-                    <Image
-                        className={'w-full aspect-video object-cover rounded-lg'}
-                        src={imagenDesktop}
-                        fill
-                        sizes={'(max-width) 100%'}
-                        alt={nombre}
-                    />
-
-                </m.div>}
-            </LazyMotion>
-            <h2
-                className={'text-xl m-0'}
+        <Grid item xl={3} lg={4} md={6} sm={6} xs={12}>
+            <motion.article
+                layoutId={`${id}`}
+                className={'relative z-10 w-full p-3 rounded-2xl cursor-pointer'}
+                ref={ref}
+                onClick={onClick}
             >
-                {nombre}
-            </h2>
-        </motion.article>
+                <LazyMotion features={domAnimation}>
+                    {isInView && <m.div
+                        className={'relative w-full aspect-video'}
+                        variants={container}
+                        initial="hidden"
+                        animate={isInView ? "show" : ''}
+                    >
+                        <Image
+                            className={'w-full aspect-video object-cover rounded-lg'}
+                            src={imagenDesktop}
+                            fill
+                            sizes={'(max-width) 100%'}
+                            alt={nombre}
+                        />
+
+                    </m.div>}
+                </LazyMotion>
+                <h2
+                    className={'text-xl m-0'}
+                >
+                    {nombre}
+                </h2>
+            </motion.article>
+        </Grid>
     )
 }
